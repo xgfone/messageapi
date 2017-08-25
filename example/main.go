0cd07865e9cc53e810072647db6cf32061453e58
xiegaofeng <xiegaofeng@grandcloud.cn>
2017-08-25 14:44:27 +0800

    Update the example.

--- example/main.go
+++ example/main.go
@@ -11,22 +11,13 @@ func main() {
 	flag.Parse()
 	c := app.NewDefaultConfig("")
 	c.AllowGet = true // Allow to use the GET method to send the message
-	// c.Emails = map[string]map[string]string{
-	// 	"plain": map[string]string{
-	// 		"host":     "mail.example.com",
-	// 		"port":     "25",
-	// 		"username": "username",
-	// 		"password": "password",
-	// 		"from":     "username@example.com",
-	// 	},
-	// }
 	c.Emails = map[string]map[string]string{
 		"plain": map[string]string{
-			"host":     "mail.grandcloud.cn",
+			"host":     "mail.example.com",
 			"port":     "25",
-			"username": "xiegaofeng",
-			"password": "XieGaoFeng",
-			"from":     "xiegaofeng@grandcloud.cn",
+			"username": "username",
+			"password": "password",
+			"from":     "username@example.com",
 		},
 	}
 	glog.Error(app.Start(c, ":8080", "", ""))
